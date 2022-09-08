57cb5c888cfebaf66c7ca90b8b0def383a222db6
Rahul Raveendran <rahulraveendran@Rahuls-MacBook-Air.local>
2022-09-08 18:10:43 +0530

    Initial commit

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,31 @@
+import { Fragment, useContext, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import ProductCard from '../../components/product-card/product-card.component';
+import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { CategoryContainer, CategoryTitle } from './category.styles';
+
+const Category = () => {
+    const { category } = useParams();
+    console.log('render/re-rendering category component');
+    const categoriesMap = useSelector(selectCategoriesMap);
+    const [products, setProducts] = useState(categoriesMap[category]);
+
+    useEffect(() => {
+        console.log('effect fired calling setProducts');
+        setProducts(categoriesMap[category]);
+    }, [category, categoriesMap]);
+
+    return (
+        <Fragment>
+            <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
+            <CategoryContainer>
+            {
+                products && products.map((product) => <ProductCard key={product.id} product={product} />)
+            }
+            </CategoryContainer>
+        </Fragment>
+    );
+};
+
+export default Category;
